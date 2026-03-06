b9828f647d72041acc26ce3de154ff97162a2b41
dfjvieira-rgb <dfjvieira@gmail.com>
2026-03-06 18:32:02 -0300

    Update and rename firebase-config1.js to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,16 @@
-// firebase-config.js
+// firebase-config.js - CONFIGURAÇÃO MASTER OAB (DIRECIONADA PMDF)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB" + "..." , // Pegue sua chave API (Web API Key) no Console do Firebase > Configurações do Projeto
+  apiKey: "AIzaSyB" + "...", // Sua chave API
   authDomain: "masteroab-db5e1.firebaseapp.com",
   databaseURL: "https://masteroab-db5e1-default-rtdb.firebaseio.com",
   projectId: "masteroab-db5e1",
   storageBucket: "masteroab-db5e1.appspot.com",
   messagingSenderId: "620234160245",
-  appId: "1:620234160245:web:..." // Pegue seu AppId no Console do Firebase
+  appId: "1:620234160245:web:..." 
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exporta o banco de dados (Realtime Database)
 export const db = getDatabase(app);
